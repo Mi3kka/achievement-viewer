600086c497860d7e55f31b94b168faf6f032e8f5
Julius Virtanen <juemiv@utu.fi>
2023-05-28 16:26:00 +0300

    Added Github logo and link
    Added Github logo and link to the footer component

--- app/footer.js
+++ app/footer.js
@@ -1,12 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Footer.css';
+import Image from 'next/image';
 
 export default function Footer() {
   return (
     <footer className="footer text-center">
       <div className="container">
-        <span className="text-muted">Created by <strong>Julius Virtanen</strong> and <strong>Anniina Talja</strong><br />
+        <span className="text-muted">Created by <strong>Julius Virtanen</strong> and <strong>Anniina Talja</strong> <br />
         This web app uses <strong>Next.js</strong> and <strong>Bootstrap</strong> to render components.<br /></span>
+        <a href="https://github.com/Mi3kka/achievement-viewer" target="_blank" rel="noopener noreferrer">
+          <Image src="/icons/github-mark-white.svg" alt="GitHub Icon" width={40} height={40} className="git-link" />
+        </a>
       </div>
     </footer>
   );
