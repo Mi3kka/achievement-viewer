3c96c3ae6cd01fbeb7c5568329b34e9aaa7ee111
Julius Virtanen <juemiv@utu.fi>
2023-05-28 23:56:30 +0300

    Added completion rates & colors
    Added completion rates & completion rate colors for the game cards

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -3,6 +3,22 @@ import Image from 'next/image';
 import '../../styles/GameCard.css';
 
 export default function GameCard({ game }) {
+  const completionPercentage = Math.floor((game.achievements.completed / game.achievements.total) * 100);
+
+  // Calculate the color based on completion rate
+  let percentageColor;
+  if (completionPercentage <= 20) {
+    percentageColor = "#ff8080"; // pastel red
+  } else if (completionPercentage <= 40) {
+    percentageColor = "#ffcc80"; // pastel orange
+  } else if (completionPercentage <= 60) {
+    percentageColor = "#ffff80"; // pastel yellow
+  } else if (completionPercentage <= 80) {
+    percentageColor = "#ccff80"; // pastel light green
+  } else {
+    percentageColor = "#80ffaa"; // pastel green
+  }
+
   return (
     <a href={`/games/${game.id}`} className="text-decoration-none text-light">
       <div className="d-flex align-items-center bg-dark rounded px-3 py-2 mb-3 game-card">
@@ -18,8 +34,8 @@ export default function GameCard({ game }) {
           <p className="game-achievement">{game.achievements.completed} / {game.achievements.total}</p>
           <p className="last-played">{game.lastPlayed ? `Last Played: ${game.lastPlayed.toISOString().slice(0,10)}` : 'Never Played'}</p>
         </div>
-        <div className="ms-auto achievement-total-container">
-          <span className="achievement-total">{game.achievements.total}</span>
+        <div className="ms-auto achievement-total-container" style={{backgroundColor: percentageColor}}>
+          <span className="achievement-total">{completionPercentage}%</span>
         </div>
         {game.achievements.completed === game.achievements.total &&
           <div className="ms-2 game-trophy">
